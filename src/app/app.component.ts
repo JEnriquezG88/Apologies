83e3b54a9a78974cd4f735b5b774975e1754b5cb
Juan Enriquez <JEnriquezG88@gmail.com>
2024-10-01 23:22:44 -0500

    Sniper component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SniperComponent } from "./components/sniper/sniper.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SniperComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/sniper/sniper.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sniper',
+  standalone: true,
+  imports: [],
+  templateUrl: './sniper.component.html',
+  styleUrl: './sniper.component.css'
+})
+export class SniperComponent {
+
+}
